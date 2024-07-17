3210ac58ff937b463507e8cc0b2f1b4b0d61be26
Tony <kanyianthony79@gmail.com>
2024-07-17 15:17:05 +0300

    update commit

--- script.js
+++ script.js
@@ -5,28 +5,29 @@ fetch (url)
 .then(response => response.json())
 .then(films => 
   films.map(film=>{
+  // console.log(film.id)
   const title= document.createElement("li")
-  addEventListener('click',()=> movie_desc(film.id))
+  title.addEventListener('click',()=> { movie_desc(film.id) })
   title.innerHTML = film.title
-  main.appendChild(title)
-
-
-
-  
+  main.appendChild(title) 
+   
 }) 
 )
 function movie_desc(filmId){
-  fetch ("http://localhost:3000/films")
-  .then(response => response.json)
-  .then (films=>{
+  console.log("----",filmId)
+  fetch ("http://localhost:3000/films/"+filmId)
+  .then(response => response.json())
+  .then (film=>{
+    console.log(film)
     const description=document.querySelector("#movie-details")
-    description.innerHTML=`<h1>${films.title}</h1>
+    description.innerHTML=`<h1>${film.title}</h1>
   <div>
-  <p>${films.description}</p>
+  <img src="${film.poster}"/>
+  <p>${film.description}</p>
   </div>
-  <h2>The movie starts at : ${films.showtime}</h2>
-  <h3>There are ${films.capacity - films.tickets_sold} tickets left</h3>
-  <img src="${films.poster}"/>
+  <h2>The movie starts at : ${film.showtime}</h2>
+  <h3>There are ${film.capacity - film.tickets_sold} tickets left</h3>
+  
   `
 
 
